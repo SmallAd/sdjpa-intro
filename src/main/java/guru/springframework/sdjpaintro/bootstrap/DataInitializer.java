b90ce79430e2601153bdfc5a5e62ac3fbfe9bf78
Vadim Piontkovskiy <vpiontkovs@wiley.com>
2022-03-15 15:58:35 +0300

    adding mysql integration test

--- src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
@@ -3,12 +3,14 @@ package guru.springframework.sdjpaintro.bootstrap;
 import guru.springframework.sdjpaintro.domain.Book;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by vpiontkovs on 3/11/2022.
  */
 @Component
+@Profile({"local", "default"})
 public class DataInitializer implements CommandLineRunner {
 
     private final BookRepository bookRepository;
@@ -19,23 +21,21 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        long count = bookRepository.count();
-        if (count == 0) {
-            Book bookDDD = new Book("Domain Driven Design", "123", "Random");
+        bookRepository.deleteAll();
+        Book bookDDD = new Book("Domain Driven Design", "123", "Random");
 
-            System.out.println("Id: " + bookDDD.getId());
+        System.out.println("Id: " + bookDDD.getId());
 
-            Book savedDDD = bookRepository.save(bookDDD);
+        Book savedDDD = bookRepository.save(bookDDD);
 
-            System.out.println("Id: " + savedDDD.getId());
+        System.out.println("Id: " + savedDDD.getId());
 
-            Book bookSIA = new Book("Spring In Action", "234234", "Oriely");
-            Book savedSIA = bookRepository.save(bookSIA);
+        Book bookSIA = new Book("Spring In Action", "234234", "Oriely");
+        Book savedSIA = bookRepository.save(bookSIA);
 
-            bookRepository.findAll().forEach(book -> {
-                System.out.println("Book Id: " + book.getId());
-                System.out.println("Book Title: " + book.getTitle());
-            });
-        }
+        bookRepository.findAll().forEach(book -> {
+            System.out.println("Book Id: " + book.getId());
+            System.out.println("Book Title: " + book.getTitle());
+        });
     }
 }
