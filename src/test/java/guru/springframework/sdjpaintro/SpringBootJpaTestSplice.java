11319d68ba71f842360d3a1291cfa06d17856913
Vadim Piontkovskiy <vpiontkovs@wiley.com>
2022-03-15 11:09:57 +0300

    adding bootstrap back to config for test splice

--- src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSplice.java
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSplice.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.annotation.Commit;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
+@ComponentScan(basePackages = "guru.springframework.sdjpaintro.bootstrap")
 public class SpringBootJpaTestSplice {
 
     @Autowired
@@ -27,7 +29,7 @@ public class SpringBootJpaTestSplice {
     @Test
     void testJpaTestSplice() {
         long countBefore = bookRepository.count();
-        assertThat(countBefore).isEqualTo(0);
+        assertThat(countBefore).isEqualTo(2);
 
         bookRepository.save(new Book("My Book", "12345", "Self"));
 
@@ -40,6 +42,6 @@ public class SpringBootJpaTestSplice {
     @Test
     void testJpaTestSpliceTransaction() {
         long countBefore = bookRepository.count();
-        assertThat(countBefore).isEqualTo(1);
+        assertThat(countBefore).isEqualTo(3);
     }
 }
